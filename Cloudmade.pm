f96f5911994ac000f65401be9f4ab5381119e87c
Alistair Francis <friffin@cpan.org>
2009-11-19 15:59:37 +0000

    Import of FRIFFIN/Geo-Coder-Cloudmade-0.2 from CPAN.
    gitpan-cpan-distribution: Geo-Coder-Cloudmade
    gitpan-cpan-version:      0.2
    gitpan-cpan-path:         FRIFFIN/Geo-Coder-Cloudmade-0.2.tar.gz
    gitpan-cpan-author:       FRIFFIN
    gitpan-cpan-maturity:     released

--- Cloudmade.pm
+++ Cloudmade.pm
@@ -1,6 +1,6 @@
 package Geo::Coder::Cloudmade;
 
-our $VERSION = '0.1';
+our $VERSION = '0.2';
 
 use strict;
 
@@ -31,7 +31,9 @@ sub new {
 
 sub geocode {
     my $self = shift;
-    my $location = shift;
+    my $args = shift;
+
+    my $location = $args->{location};
 
     if( Encode::is_utf8($location) ) {
         $location = Encode::encode_utf8($location);
@@ -68,3 +70,55 @@ sub geocode {
 
 __END__
 
+=head1 NAME
+
+Geo::Coder::Cloudmade - Geocode addresses with the Cloudmade API 
+
+=head1 VERSION
+
+Version 0.2
+
+=head1 SYNOPSIS
+
+Provides a thin Perl interface to the Cloudmade Geocoding API.
+
+    use Geo::Coder::Cloudmade;
+
+    my $geocoder = Geo::Coder::Cloudmade->new( apikey => 'my_app' );
+    my $location = $geocoder->geocode( { location => '1370 Willow Road, 2nd Floor, Menlo Park, CA 94025 USA' } );
+
+=head1 OFFICIAL API DOCUMENTATION
+
+Read more about the API at
+L<http://developers.cloudmade.com/>.
+
+=head1 METHOD
+
+=over 4
+
+=head2 new   
+
+Constructs a new C<Geo::Coder::Cloudmade> object and returns it. Requires a 
+Cloudmade api key as an argument.
+
+  KEY                   VALUE
+  -----------           --------------------
+  apikey                Cloudmade API key
+
+
+=head2 geocode
+
+Takes a location in a hashref as an argument and returns the list of matching
+coordinates for the specified location.
+
+=head1 AUTHOR
+
+Alistair Francis, http://search.cpan.org/~friffin/
+
+=head1 COPYRIGHT AND LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.10 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
